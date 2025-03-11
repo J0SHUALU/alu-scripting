b59e75cb78bba2653ab81d2756a5d19fc2afa7d6
Joshualu <j.moses@alustudent.com>
2025-03-11 19:33:51 +0200

    0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""DOCUMENTATION"""
+"""
+Function that uses the Reddit API and returns the
+number of subscribers for a given subreddit on Reddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOCUMENTATION"""
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """
+    Takes a subreddit name, and returns the number of subscribers
+    new line
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
